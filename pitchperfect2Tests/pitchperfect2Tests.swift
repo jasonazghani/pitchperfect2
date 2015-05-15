237abafded4f5a0480da58624639ff92b709f412
jason <jasonazghani@me.com>
2015-05-14 19:35:49 -0700

    Initial Commit

---
+++ pitchperfect2Tests/pitchperfect2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  pitchperfect2Tests.swift
+//  pitchperfect2Tests
+//
+//  Created by jasonazghani on 5/14/15.
+//  Copyright (c) 2015 jasonazghani. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class pitchperfect2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
